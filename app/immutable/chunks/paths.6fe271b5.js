4002981088197e3f2d5e7891ad80cfeb160c37c7
vivien-cmtifi <vivien@commutifi.com>
2026-02-26 12:48:54 -0500

    Updates

---
+++ app/immutable/chunks/paths.6fe271b5.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_iet3a2)==null?void 0:s.base)??"/agefo-trousse-dev";var e;const t=((e=globalThis.__sveltekit_iet3a2)==null?void 0:e.assets)??a;export{t as a,a as b};
